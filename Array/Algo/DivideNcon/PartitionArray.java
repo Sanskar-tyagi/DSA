94f661f45af960777b4b5d1baf658fcbbede8e8c
Sanskar Tyagi <Sanskarbhartityagi@gmail.com>
2023-03-01 23:19:42 +0530

    Revert "D-18 Quick Sort,MaxSum,UniquePAthsum ,Remove K"

--- Array/Algo/DivideNcon/PartitionArray.java
+++ Array/Algo/DivideNcon/PartitionArray.java
@@ -1,31 +1,59 @@
 package Array.Algo.DivideNcon;
 
+import java.util.Scanner;
+
 public class PartitionArray {
-    // part the given array into smaller and larger numbers at K pivot
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("n");
+        int n = sc.nextInt();
+        System.out.println("K");
+        int k = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        maxSumAfterPartitioning(arr, k);
+
     }
 
-    static void Lnc(int arr[], int k) {
-        int i = 0;
-        int j = 0;
-        while (j != arr.length || i != arr.length) {
-            if (arr[i] <= k) {
-                swap(arr, i, j);
-                i++;
-                j++;
-            } else {
-                j++;
+    public static void maxSumAfterPartitioning(int[] arr, int k) {
+        int idx = 0;
+        int r = Integer.MIN_VALUE;
+        int l = Integer.MIN_VALUE;
+        while (idx != k) {
+            if (arr[idx] > l) {
+                l = arr[idx];
             }
+            idx++;
+        }
+        while (idx != arr.length) {
+            if (arr[idx] > r) {
+                r = arr[idx];
+            }
+            idx++;
+        }
+        System.out.println("ar");
+        for (int i = 0; i < arr.length; i++) {
+
+            System.out.print(arr[i]);
+        }
+        swapall(arr, 0, l, k - 1);
+        swapall(arr, k + 1, r, arr.length);
+        int sum = 0;
+        System.out.println("rr");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
         }
 
     }
 
-    static void swap(int arr[], int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-
+    static void swapall(int arr[], int idx, int i, int r) {
+        while (idx < r) {
+            arr[idx] = i;
+            idx++;
+        }
     }
 }
