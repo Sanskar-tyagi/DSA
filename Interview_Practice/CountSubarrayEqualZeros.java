3d38bcdda5af4461318abdd028d635da0f5b9b37
Sanskar Tyagi <Sanskarbhartityagi@gmail.com>
2023-07-16 23:05:44 +0530

    t-44

--- Interview_Practice/CountSubarrayEqualZeros.java
+++ Interview_Practice/CountSubarrayEqualZeros.java
@@ -1,5 +1,13 @@
 package Interview_Practice;
 
+import java.util.HashMap;
+
 public class CountSubarrayEqualZeros {
-    
+    // ! Question
+    // ->https://www.geeksforgeeks.org/count-subarrays-equal-number-1s-0s/
+
+    // Intiution
+
+    HashMap<Integer, Integer> map = new HashMap<>();
+
 }
